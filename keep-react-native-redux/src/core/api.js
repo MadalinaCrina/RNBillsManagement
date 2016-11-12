26932a594d5415c5e5fa0c04b55f0aa26e25495e
Todoran Madalina <todoran_madalina@yahoo.com>
2016-11-13 01:46:02 +0200

    Deleting unused files

--- keep-react-native-redux/src/core/api.js
+++
@@ -1,4 +0,0 @@
-// export const apiUrl = 'http://192.168.1.10:3000/api';
-export const apiUrl = 'http://192.168.0.129:3000/api';
-export const headers = {'Accept': 'application/json', 'Content-Type': 'application/json'};
-export const authHeaders = (token) => ({...headers, 'Authorization': `Bearer ${token}`});
--- keep-react-native-redux/src/note/index.js
+++
@@ -1,3 +0,0 @@
-export {NoteList} from './NoteList';
-export {NoteEdit} from './NoteEdit';
-export {noteReducer} from './service';
